c0d5ddbad3707cb22856b41dffb38f12289423c6
Saveliy Kochanov <kochanov.saveliy222@mail.ru>
2024-06-22 17:36:01 +0700

    Second sent to review(hot fix)

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,29 @@ const placesContainer = document.querySelector('.places__list')
 function createCard(cardName, cardImageLink) {
 	const cardTemplate = document.querySelector('#card-template').content
 	const cardElement = cardTemplate.querySelector('.card').cloneNode(true)
+	const cardElementImage = cardElement.querySelector('.card__image')
 
-	cardElement.querySelector('.card__image').setAttribute('src', cardImageLink);
-	cardElement.querySelector('.card__image').setAttribute('alt', cardName);
+	cardElementImage.setAttribute('src', cardImageLink)
+	cardElementImage.setAttribute('alt', cardName)
 	cardElement.querySelector('.card__title').textContent = cardName
 
 	const deleteButton = cardElement.querySelector('.card__delete-button')
 	deleteButton.addEventListener('click', () => {
-		cardElement.remove()
+		deleteCard(cardElement)
 	})
 
+	return cardElement
+}
+
+function deleteCard(cardElement) {
+	cardElement.remove()
+}
+
+function addCard(cardElement) {
 	placesContainer.append(cardElement)
 }
 
 initialCards.forEach(card => {
-	createCard(card.name, card.link)
+	const newCard = createCard(card.name, card.link)
+	addCard(newCard)
 })
